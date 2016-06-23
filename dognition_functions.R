9a7a7d5352fd49ecf9c9b471d2a7800f96d151a4
Jade <yzh828@berkeley.edu>
2016-06-23 13:22:50 -0700

    analysis finished

--- dognition_functions.R
+++ dognition_functions.R
@@ -1,7 +1,61 @@
-time_slot <- function(hour,day,month,day_sect=2){#make a distinct code for each month-day of the week-hour of the day combination
+#make a distinct code for each month-day of the week-hour of the day combination
+time_slot <- function(hour,day,month,day_sect=2){
 	time <- month*10 + day 
 	for(i in 1:(day_sect-1)){
 		time <- time + 1/day_sect*(hour > 24/day_sect*i)
 	}
 	time
+}
+
+#for a given feature, generate a table each row of which is the frequency distribution among time slots at the corresponding level of the feature
+feature_time_table <- function(feature, time){
+	values <- sort(unique(feature))
+	result <- t(matrix(unlist(lapply(values,time_profile,feature=feature,time=time)),ncol=length(values)))
+	colnames(result) <- sort(unique(time))
+	rownames(result) <- values
+	result
+}
+
+#for a given level of a feature, returns the frequency distribution among time slots
+time_profile <- function(v,feature,time){
+	nt <- sort(unique(time))
+	profile <- numeric(length(nt))
+	for(i in 1:length(nt)){
+		profile[i] <- sum((feature==v)*(time==nt[i]))
+	}
+	profile
+}
+
+#for given values of all features, returns the frequency distribution among time slots (weight-averaged over all features)
+get_trend <- function(vs,ftables,wts,average){
+	wts <- wts/sum(wts)
+	trend <- numeric(dim(ftables[[1]])[2])
+	for(i in 1:length(vs)){
+		rftable <- ftables[[i]]/rowSums(ftables[[i]])
+		index <- match(vs[i],rownames(rftable))
+		if(!is.na(index)){
+			trend <- trend+wts[i]*rftable[index,]
+		}else{
+			trend <- trend+wts[i]*average
+		}
+	}
+	trend
+}
+
+#plot the time distribution
+plot_trend <- function(trend,average,graph,index){
+	time <- 1:12
+	if(graph){
+		plot(trend~time,xlab="month",ylab="activity",ylim=c(0,0.5),yaxt='n',col=0)
+		lines(time,average,lty=2,col=1)
+	}
+	lines(time,trend,lty=1,col=index)
+}
+
+#calculate similarity between two time distributions
+trend_similarity <- function(trend1,trend2,average){
+	simi <- cor(trend1,trend2)
+	#compare to average; similarity should be 0.5 when compared to average
+	contra <- min(cor(trend2,average),cor(trend1,average))
+	0.5*(simi-contra)/(1-contra)+0.5
 }
--- main.R
+++ main.R
@@ -1,32 +1,77 @@
 library(randomForest)
 source("dognition_functions.R")
 
-tests <- read.csv("test_complete.csv",header=T)
-
-dogs <- read.csv("dog_list.csv",header=T)
-
 ##I. Which dog feature(s) best determine dog activity pattern?
 
-#combine month, day and hour into one variable
+tests <- read.csv("test_complete.csv",header=T)
 
-dog_tests <- data.frame(time=as.factor(time_slot(0,0,tests$month,day_sect=2)),tests[,c(6:13)])
+#Preprocessing
+tests <- tests[!is.na(tests$neutered),]
 
-#to see initiation time
-test_dogs <- data.frame(time=as.factor(time_slot(0,0,dogs$month,day_sect=2)),dogs[,c(6:13)])
-
-#Only month can be more accurately predicted; combined with before or after noon is also fine.
+tests$time <- as.factor(time_slot(tests$hour,tests$day,tests$month,day_sect=2))
 
 #random forest classification
-rf <- randomForest(time~gender+weight+personality+age+weight+btype+bgroup,data=dog_tests,ntree=100,importance=T)
-mean(rf$err.rate)
-
-#no pattern! initiation is random.
-rfd <- randomForest(time~gender+weight+personality+age+weight+btype+bgroup,data=test_dogs,ntree=100,importance=T)
-mean(rfd$err.rate)
+rf <- randomForest(time~gender+neutered+personality+weight+age+btype+bgroup,data=tests,ntree=100,importance=T)
 
 #show variable importance (decrease in explained variation after permutation)
 round(importance(rf,type=1,scale=T),2)
 
-##II. How similar in activitiy pattern are two dogs given their features?
+#Only month can be more accurately predicted; combined with before or after noon is also fine.
 
-#For each dog feature, create a pmi table (row: feature classes, column: time chunks) and calculate pmi among classes
+##II. Which dog features determine initiation timing and completion rate?
+
+dogs <- read.csv("dog_list.csv",header=T)
+dogs <- dogs[!is.na(dogs$neutered),]
+dogs <- data.frame(time=as.factor(time_slot(0,0,dogs$month,day_sect=2)),dogs[,c(6:15)])
+
+rfinit <- randomForest(time~gender+neutered+personality+age+weight+btype+bgroup,data=dogs,ntree=100,importance=T)
+mean(rfinit$err.rate)
+
+rfncomp <- randomForest(ntests~gender+neutered+weight+personality+age+btype+bgroup,data=dogs,ntree=100,importance=T)
+rfncomp
+
+lmncomp <- glm(ntests~gender+neutered+weight+personality+age+btype+bgroup,data=dogs)
+summary(lmncomp)
+
+#no pattern! initiation and completion rate are random.
+
+##III. How similar in activitiy pattern are two dogs given their features?
+
+#Make frequency tables for each feature (rows: feature levels; columns: month)
+tests$age <- as.factor(c("0-1","2-4","5-7","7-10",">10"))[(tests$age>=0)+(tests$age>1)+(tests$age>4)+(tests$age>7)+(tests$age>10)]
+
+tests$weight <- as.factor(c("0-19","20-49","50-89","90-139",">139"))[(tests$weight>=0)+(tests$weight>19)+(tests$weight>49)+(tests$weight>89)+(tests$weight>129)]
+
+rf <- randomForest(as.factor(month)~gender+neutered+personality+weight+age+btype+bgroup,data=tests,ntree=100,importance=T)
+
+feature_wts <- round(importance(rf,type=1,scale=T),2)
+
+ftables <- list()
+features <- c("gender","neutered","weight","personality","age","btype","bgroup")
+for(i in 1:length(features)){
+	ftables[[i]] <- feature_time_table(tests[,names(tests)==features[i]], tests$month)
+}
+names(ftables) <- features
+
+#calculate the overall time distribution
+average <- colSums(ftables[[4]])/dim(ftables[[4]])[1]
+average <- average/sum(average)
+
+#Given values for all features, generate a time trend (weighted sum over all features)
+vs1 <- c("female","1","0-19","ace","0-1","Pure Breed","Working")
+trend1 <- get_trend(vs1,ftables,feature_wts,average)
+
+vs2 <- c("male","0",">139","stargazer","7-10","Cross Breed","Toy")
+trend2 <- get_trend(vs2,ftables,feature_wts,average)
+
+vs3 <- c("female","1",">139",NA,"7-10","Pure Breed","Working")
+trend3 <- get_trend(vs3,ftables,feature_wts,average)
+
+plot_trend(trend1,average,graph=T,index=1)
+plot_trend(trend2,average,graph=F,index=2)
+plot_trend(trend3,average,graph=F,index=3)
+legend("topright",c(paste("dog",1:3),"average"),lty=c(rep(1,3),2),col=c(1:3,1))
+
+trend_similarity(trend1,trend2,average)
+trend_similarity(trend3,trend2,average)
+trend_similarity(trend1,trend3,average)
